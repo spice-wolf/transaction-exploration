a80587160af7e799ccea96b6b069883ea3f0b276
spice <17806757663@163.com>
2021-05-10 11:38:29 +0800

    feat(src):初始化项目

---
+++ src/main/java/com/spice/TransactionExplorationApplication.java
@@ -0,0 +1,16 @@
+package com.spice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author spice
+ * @date 2021/5/10 11:37
+ */
+@SpringBootApplication
+public class TransactionExplorationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionExplorationApplication.class, args);
+    }
+}
---
+++ src/test/java/com/spice/TransactionExplorationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author spice
+ * @date 2021/5/10 11:37
+ */
+@SpringBootTest
+class TransactionExplorationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
